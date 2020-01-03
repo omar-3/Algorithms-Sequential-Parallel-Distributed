a4fc12ce95225dd437b498e42f144d3274e1947c
Omar <s-omarkhaled@zewailcity.edu.eg>
2020-01-03 23:22:54 +0200

    added bash

---
+++ ch1/Naive_Powers/algorithm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+naive_power () {
+    number=$1
+    power=$2
+    product=$number
+    for (( i=1; i<$power;i++ )); do
+        product=$(($product*$number));
+    done
+    echo $product
+}
+
+naive_power "5" "3"
