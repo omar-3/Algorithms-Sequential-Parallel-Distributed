a3aef3c045dd69b2b5134141c03fdb50496740a6
Okhaledzaki <s-omarkhaled@zewailcity.edu.eg>
2020-02-07 14:19:12 +0200

    bash is working now

--- ch2/Insertion_Sort/algorithm.sh
+++ ch2/Insertion_Sort/algorithm.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 insertion_sort() {
     for (( i=1; i<${#a[@]}; i++ )); do
-        local firstElement=$((i-1))
+        local position=$(( i-1 ))
         local toBeRotated=${a[$i]}
-        while  [[ $firstElement -ge 0 &&  ${a[$firstElement]} -gt ${a[$i]} ]]
+        while  [[ $position -ge 0 &&  ${a[$position]} -gt $toBeRotated ]]
         do
-            a[$(($firstElement+1))]=${a[$firstElement]}
-            firstElement=$(($firstElement - 1))
+            a[$(( position+1 ))]=${a[$position]}
+            position=$(( position - 1 ))
         done
-        firstElement=$(($firstElement + 1))
-        a[$firstElement]=$toBeRotated
+        position=$(( position + 1 ))
+        a[$position]=$toBeRotated
         echo ${a[@]}
     done
     echo ${a[@]}
 }
 
 
-a=(5 4 3 2 1)
-insertion_sort 
+a=(5 4 3 2 10 15 19 10 8 4 21 39 82 71 31 1 -2 -1 -5 0 1)
+insertion_sort | nl
