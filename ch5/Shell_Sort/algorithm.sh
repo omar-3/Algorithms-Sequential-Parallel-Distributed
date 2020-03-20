4cb8fce30e32e69dfdca55f3d751bc330602c324
omar659 <s-omarkhaled@zewailcity.edu.eg>
2020-03-20 15:38:04 +0200

    bash is buggy

---
+++ ch5/Shell_Sort/algorithm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+InsertionSubSort() {
+    local h=$1
+    # echo $h
+    for (( i=h; i<${#a[@]}; i++ ))
+    do
+        local current=${a[$i]}
+        local position=$(( i - h ))
+        while [[ $position -ge 0 && ${a[$position]} -gt $current ]]
+        do
+            a[$(( position + h ))]=${a[$position]}
+            position=$(( position - h ))
+        done
+        a[$(( position + h))]=$current
+    done
+}
+
+ShellSort() {
+    for (( i=0; i< 6; i++ ))
+    do
+        InsertionSubSort ${k[$i]}
+        echo "lol"
+    done
+    # echo ${a[@]}
+}
+
+a=(5 4 3 2 1 10 13 19 0 0 4 21 43 71 32 1 -9)
+k=(6 5 4 3 2 1)
+
+ShellSort | nl
