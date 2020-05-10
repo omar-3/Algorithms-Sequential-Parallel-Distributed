338035e6a5b26205b5d26740a6095d688db57199
omar-3 <s-omarkhaled@zewailcity.edu.eg>
2020-05-10 23:47:45 +0200

    added golang

--- ch8/Naive_PolyMult/algorithm.go
+++ ch8/Naive_PolyMult/algorithm.go
@@ -1,1 +1,40 @@
 package main
+
+import "fmt"
+
+func naive_poly_mult(P map[int]int, Q map[int]int) map[int]int {
+	M := map[int]int{}
+	for keyP, valueP := range P {
+		for keyQ, valueQ := range Q {
+			if _, ok := M[keyP+keyQ]; ok {
+				M[keyP+keyQ] = M[keyP+keyQ] + valueP*valueQ
+			} else {
+				M[keyP+keyQ] = valueP * valueQ
+			}
+		}
+	}
+	return M
+}
+
+func main() {
+	P := map[int]int{
+		0: -5,
+		1: 2,
+		2: 3,
+	}
+
+	Q := map[int]int{
+		0: 4,
+		1: -1,
+		2: 0,
+		3: 1,
+	}
+
+	M := naive_poly_mult(P, Q)
+
+	for key, value := range M {
+		fmt.Println(key)
+		fmt.Println(value)
+		fmt.Println("------------")
+	}
+}
